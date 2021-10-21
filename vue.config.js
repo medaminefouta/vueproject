bcfa7ab7271a156ee577ea50dba11fceefd3f1e4
medaminefouta <76180018+medaminefouta@users.noreply.github.com>
2021-10-21 13:08:51 +0200

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+  /*devServer: {
+    disableHostCheck: true,
+    port: 8081,
+    public: '0.0.0.0:8081'
+},
+publicPath: "/"*/
+}
